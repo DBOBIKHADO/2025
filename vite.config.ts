ef685b0d25d30690c372b53255a8e61d4f58e9a2
DBOBIKHADO <bikhado@gmail.com>
2025-06-25 18:22:41 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./client/src"),
-      "@shared": path.resolve(__dirname, "./shared"),
-    },
-  },
-  root: "./client",
+  root: "client",
   build: {
-    outDir: "../dist/public",
+    outDir: "../dist",
     emptyOutDir: true,
   },
 });
